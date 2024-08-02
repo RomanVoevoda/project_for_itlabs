d47820092264d96ab6523b1957d65dad6455a093
Roman Druzhinin <karamount11@gmail.com>
2024-08-02 16:49:28 +0300

    Инициализация проекта
    *Настроены webpack и typescript
    *Добавлены общие стили, шрифты, настроена цветовая схема по макету и размеры переменных(не адаптивные)

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import classes from './App.module.scss';
+
+const App: FC = () => {
+  return (
+    <div className={classes.app}>Appp</div>
+  );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,28 @@
+import {buildWebpack} from "./config/build/buildWebpack";
+import webpack from 'webpack';
+import { BuildMode, BuildPaths } from "./config/build/types/types";
+import path from 'path';
+
+interface EnvVariables {
+	mode: BuildMode;
+  analyzer?: boolean;
+  port: number;
+}
+
+export default (env: EnvVariables) => {
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, 'build'),
+    entry: './src/index.tsx',
+    html: 'public/index.html',
+    src: path.resolve(__dirname, 'src')
+  }
+
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? 'development',
+    paths,
+    analyzer: env.analyzer
+  });
+	
+	return config;
+};
